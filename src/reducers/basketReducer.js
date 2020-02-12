5c3982ee3afd3f694aaaeec7e7162e4a43102cf2
Donald David Sinclair-Smith <donalddavidsinclair-smith@donalds-imac.home>
2020-02-12 17:11:41 +0000

    remove a console.log

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -45,7 +45,6 @@ const basketReducer = (state = initState, action) => {
     let itemToRemove = state.addedItems.find(item => action.id === item.id)
     let new_items = state.addedItems.filter(item => action.id !== item.id)
     let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity)
-    console.log(itemToRemove)
     return {
       ...state,
       addedItems: new_items,
